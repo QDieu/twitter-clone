5581767bfe4d4e603ffe92b40df3299f54e3d248
qdieu <navvy3@gmail.com>
2022-02-17 00:22:20 +0200

    ...

--- src/theme.ts
+++ src/theme.ts
@@ -1,4 +1,3 @@
-//@ts-nocheck
 import { createTheme } from '@mui/material';
 import { red } from '@mui/material/colors';
 
@@ -13,10 +12,78 @@ export const theme = createTheme({
             'Ubuntu',
             'Helvetica Neue',
             'sans-serif',
-        ],
+        ].join(','),
+    },
+    components : {
+        MuiButton: {
+            styleOverrides : {
+                root : {
+                    borderRadius : 30,
+                    textTransform : 'none',
+                    fontSize : 16,
+                    height : 40,
+                    fontWeight : 700,
+                },
+                textPrimary: {
+                    paddingLeft: 20,
+                    paddingRight: 20,
+                },
+                outlinedPrimary: {
+                    borderColor: 'rgb(29,161,243)',
+                },
+            }
+        },
+        MuiFilledInput : {
+            styleOverrides:{ 
+                underline : {
+                    '&:after' : {
+                        borderBottomWidth : '2px',
+                    },
+                    '&:before':{
+                        borderColor : "#000",
+                        borderBottomWidth: '2px'
+                    }
+                },
+                input : {
+                    backgroundColor : 'rgb(245,248,250)'
+                }
+            }
+        },
+        MuiDialog: {
+            styleOverrides :{
+                paper: {
+                    borderRadius: 15,
+                },
+            }
+        },
+        MuiDialogActions : {
+            styleOverrides: {
+                root: {
+                    marginBottom: 8,
+                }
+            }
+        },
+        MuiDialogTitle : {
+            styleOverrides : {
+                root : {
+                    borderBottom : '1px solid rgb(204,214,221)',
+                    marginBottom : 10,
+                    padding : '10px 15px',
+                    '& h2':{
+                        display : 'flex',
+                        alignItems :'center',
+                        fontWeight : 800,
+                    },
+                    '& button':{
+                        padding : 8,
+                        marginRight : 20,
+                    }
+                }
+            }
+        }
     },
     palette: {
-        primmary: {
+        primary: {
             main: 'rgb(29,161,242)',
             dark: 'rgb(26,145,218)',
             contrastText: '#fff',
@@ -30,65 +97,8 @@ export const theme = createTheme({
         background: {
             default: '#fff',
         },
-    },
-    shadow: [],
-    overrides: {
-        MuiButton: {
-            root: {
-                borderRadius: 30,
-                textTransform: 'none',
-                fontSize: 16,
-                height: 40,
-            },
-            textPrimary: {
-                paddingLeft: 20,
-                paddingRight: 20,
-            },
-            outlinedPrimary: {
-                borderColor: 'rgb(29,161,243)',
-            },
-        },
-        MuiFilledInput: {
-            undeline: {
-                '&:after': {
-                    borderBottomWidth: '2px',
-                },
-                '&:before': {
-                    borderColor: '#000',
-                    borderBottomWidth: '2px',
-                },
-            },
-            input: {
-                backgroundColor: 'rgb(245,248,250)',
-            },
-        },
-        MuiDialog: {
-            paper: {
-                borderRadius: 15,
-            },
-        },
-        MuiDialogActions: {
-            root: {
-                marginBottom: 8,
-            },
-        },
-        MuyDialogTitle: {
-            root: {
-                borderBottom: '1px solid rgb(204,214,221)',
-                marginBottom: 10,
-                padding: '10px 15px',
-                '& h2': {
-                    display: 'flex',
-                    alignItems: 'center',
-                    fontWeight: 800,
-                },
-                '& button': {
-                    padding: 8,
-                    marginRight: 20,
-                },
-            },
-        },
-    },
+    },    
+
 });
 
 export default theme;
